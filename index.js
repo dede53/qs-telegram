bf47ea7c057f05548e79e55553ab30af640ed610
dede53 <hallomarcel.hm@gmail.com>
2019-02-23 10:51:32 +0100

    recive images an use events

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-const TelegramBot               = require('node-telegram-bot-api');
-var adapter					    =	require('../../adapter-lib.js');
+var TelegramBot					=	require('node-telegram-bot-api');
+var fs							=	require('fs');
+var express						=	require('express.oi');
+var adapter						=	require('../../adapter-lib.js');
 var telegram					=	new adapter("telegram");
+var app							=	express().http().io();
 
-// replace the value below with the Telegram token you receive from @BotFather
-var token = '367772499:AAH37llKnAYwUzfIKqVV-k0gKj64TCd7V0Q';
+app.use(express.static(__dirname + '/dist'));
 
-process.on('message', function(data) {
-  switch(data.protocol){
-    case "setSetting":
-      telegram.setSetting(data.setSetting.name, data.setSetting.status);
-      break;
-    case "send":
-      sendMessage(data);
-      break;
-    default:
-      telegram.log.error(data);
-      break;
-  }
+telegram.on('telegram', function(data) {
+	switch(data.protocol){
+		case "setSetting":
+			telegram.log.debug("process.on.settSetting");
+			telegram.setSetting(data.setSetting.name, data.setSetting.status);
+			break;
+		case "send":
+			sendMessage(data);
+			break;
+		default:
+			telegram.log.error(data);
+			break;
+	}
 });
+
+
 
 /**********************
  * 
@@ -41,39 +46,90 @@ process.on('message', function(data) {
 
 // Create a bot that uses 'polling' to fetch new updates
 try{
-    var bot = new TelegramBot(token, {polling: true});
-    process.send({"statusMessage": "Verbunden"});
+	var bot = new TelegramBot(telegram.settings.token, {polling: true});
+	process.send({"statusMessage": "Verbunden"});
 }catch(error){
-    process.send({"statusMessage": "Konnte keine Verbingung herstellen"});
+	process.send({"statusMessage": "Konnte keine Verbingung herstellen"});
 }
 
+createDir(__dirname + '/dist');
+
 bot.on('text', (msg) => {
-    const chatId = msg.chat.id;
-    if(chatId == telegram.settings.familyGroup){
-        var data = {};
-        data.author = msg.from.first_name;
-        data.message = msg.text;
-        if(msg.entities && msg.entities[0].type == "url"){
-             data.type = 2;
-        }else{
-             data.type = 1;
-        }
-        process.send({"chatMessage": data});
-    }else{
-      telegram.log.error(chatId);
-    }
+	const chatId = msg.chat.id;
+	if(chatId == telegram.settings.familyGroup){
+		var data = {};
+		data.author = msg.from.first_name;
+		data.message = msg.text;
+		if(msg.entities && msg.entities[0].type == "url"){
+			 data.type = 2;
+		}else{
+			 data.type = 1;
+		}
+		process.send({"chatMessage": data});
+	}else{
+	  telegram.log.error(chatId);
+	}
+});
+
+bot.on('photo', (msg) => {
+	const chatId = msg.chat.id;
+	console.log(msg);
+	if(chatId == telegram.settings.familyGroup){
+		bot.downloadFile(msg.photo[msg.photo.length - 1].file_id, __dirname + '/dist').then((fileName) => {
+			var data = {};
+			data.type		= 3;
+			data.author		= msg.from.first_name;
+			data.message	= 'http://' + telegram.settings.localIp + ':' + telegram.settings.port + '/' + fileName.split('/').pop();
+			process.send({"chatMessage": data});
+		});
+	}else{
+	  telegram.log.error(chatId);
+	}
+});
+
+bot.getMe().then( (user) => {
+	telegram.settings.botId = user.id;
+	telegram.setSettings(telegram.settings);
+});
+
+bot.on('new_chat_members', (msg) => {
+	if(msg.new_chat_participant.id == telegram.settings.botId){
+		telegram.settings.familyGroup = msg.chat.id;
+		telegram.setSettings(telegram.settings);
+		telegram.log.debug("Bot wurde in " + msg.chat.title + " eingeladen");
+	}
 });
 
 function sendMessage(data){
-    if(!data.receiver){
-      data.receiver = telegram.settings.familyGroup;
-    }
-    switch(data.type){
-        case "photo":
-            bot.sendPhoto(data.receiver, data);
-            break;
-        default:
-            bot.sendMessage(data.receiver, data.message.toString());
-            break;
-    }
+	if(!data.receiver){
+	  data.receiver = telegram.settings.familyGroup;
+	}
+	switch(data.type){
+		case "photo":
+			bot.sendPhoto(data.receiver, data);
+			break;
+		default:
+			bot.sendMessage(data.receiver, data.message.toString());
+			break;
+	}
+}
+
+function createDir(name){
+	if(!fs.existsSync(name)){
+		fs.mkdirSync(name, 0766, function(err){
+			if(err){
+				adapter.log.error("mkdir " + name + ": failed: " + err);
+			}else{
+				adapter.log.info(name + " wurde erstellt");
+			}
+		});
+	}
+}
+
+try{
+	app.listen(telegram.settings.port, function(){
+		process.send({"statusMessage": "Läut auf Port:" + telegram.settings.port});
+	});
+}catch(e){
+    telegram.log.error("Port besetzt");
 }
